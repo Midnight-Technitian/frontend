12d6ca559e8636bb727e868650a7ab591a1b2e6d
Glabay <Java_Guru@live.com>
2025-10-22 02:27:46 -0300

    Dashboard controller implementatrion
    Signed-off-by: Glabay <Java_Guru@live.com>

--- website/src/main/java/dev/glabay/controllers/DashboardController.java
+++ website/src/main/java/dev/glabay/controllers/DashboardController.java
@@ -1,10 +1,35 @@
 package dev.glabay.controllers;
 
+import dev.glabay.dtos.CustomerDto;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestClient;
+
 /**
  * @author Glabay | Glabay-Studios
  * @project frontend
  * @social Discord: Glabay
  * @since 2025-10-21
  */
+@Controller
+@RequiredArgsConstructor
 public class DashboardController {
+    private final RestClient restClient;
+
+    @GetMapping("/dashboard")
+    public String getDashboard(HttpServletRequest request, Model model) {
+        var username = request.getRemoteUser();
+        var customerDto = restClient.get()
+            .uri("/v1/customers/e/".concat(username))
+            .retrieve()
+            .toEntity(new ParameterizedTypeReference<CustomerDto>() {})
+            .getBody();
+
+        model.addAttribute("customer", customerDto);
+        return "customer/dashboard";
+    }
 }
