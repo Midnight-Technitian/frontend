e13f78fcd887ba5bcbd4a700263cac40cf2575ed
Glabay <Java_Guru@live.com>
2025-10-26 15:42:39 -0300

    Added logging to the service
    Signed-off-by: Glabay <Java_Guru@live.com>

--- website/src/main/java/dev/glabay/feaures/users/CustomUserDetailsService.java
+++ website/src/main/java/dev/glabay/feaures/users/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package dev.glabay.feaures.users;
 
+import dev.glabay.logging.MidnightLogger;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NullUnmarked;
+import org.slf4j.Logger;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,14 +18,22 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    private final Logger log = MidnightLogger.getLogger(CustomUserDetailsService.class);
+
     private final UserProfileRepository userProfileRepository;
 
     @Override
     @NullUnmarked
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var cachedUser = userProfileRepository.findByEmailIgnoreCase(username);
-        return cachedUser
-            .map(CustomUserDetails::new)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        log.info("Loading UserDetails for username: {}", username);
+        if (userProfileRepository.existsByEmail(username)) {
+            var cachedUser = userProfileRepository.findByEmailIgnoreCase(username);
+            log.info("UserDetails for username: {}", cachedUser);
+            return cachedUser
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        }
+        log.debug("No user found for for email: {}", username);
+        throw new UsernameNotFoundException("User not found: " + username);
     }
 }
